e3de4060aa7d975418db1e1a5289f9b9796fe2c7
muhammad-hassan3 <muhammadhassanvs@gmail.com>
2026-06-16 21:11:45 +0530

    first commit

---
+++ 2-comments & variable.py
@@ -0,0 +1,8 @@
+#this is my second program
+print("welcome to 2nd session")
+'''print("learn commenting")
+print("short cut for commenting is cntrl + /")'''
+name="hassan"
+age=21
+print("my name is",name)
+print("age =",age)
---
+++ 3-datatypes.py
@@ -0,0 +1,47 @@
+#day 3
+#types of data types
+
+#string data type
+name = "John"
+print("My name is", name)
+print("Data type of name:", type(name))
+
+
+
+#integer data type
+age = 25    
+print("My age is", age)
+print("Data type of age:", type(age))
+
+
+#float data type
+height = 5.9    
+print("My height is", height)
+print("Data type of height:", type(height))
+
+
+#complex data type
+complex_number = 2 + 3j
+print("My complex number is:", complex_number)
+print("Data type of complex_number:", type(complex_number))
+
+
+#boolean data type
+is_student = True   
+print("Am I a student?", is_student)
+print("Data type of is_student:", type(is_student))
+
+
+#list data type
+fruits = ["apple", "banana", "cherry"]
+print("My favorite fruits are:", fruits)
+print("Data type of fruits:", type(fruits))
+
+
+#tuple data type
+coordinates = (10, 20)  
+print("My coordinates are:", coordinates)
+print("Data type of coordinates:", type(coordinates))
+
+
+#
